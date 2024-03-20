c633a05d18725aac71bb8a79486b3f43f44dac19
Nishant Dubey <nishant.avology@gmail.com>
2024-03-20 19:23:47 +0530

    fix

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,26 +2,15 @@ import { type ClassValue, clsx } from "clsx"
 import path from "path";
 import { twMerge } from "tailwind-merge"
 
-const fs = typeof window === 'undefined' ? require('fs').promises : null;
+import { ListBlobResult, list } from '@vercel/blob';
 
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export const getbrandProfile = async () => {
-  
-  let dirs:string[] = [];
-  try {
-    // Attempt to read the directories from the specified path
-     dirs = await fs.readdir(path.join(process.cwd(), "/public/brand-style"));
 
-     console.log(dirs);
-     
-    // Return an object with the populated dirs array
-    return { dirs };
-  } catch (error) {
-    // Handle errors, and return an object with an 'error' property
-    return {dirs};
-  }
-};
+export const getItem = async () => {
+  const item = await list();
+  return item
+}
